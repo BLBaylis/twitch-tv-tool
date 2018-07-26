15f67b0aca20f65ab03c25b5d6221fca8162ea96
BLBaylis <bradleybaylis@hotmail.com>
2018-07-26 21:35:13 +0100

    Revert "Revert "Initial search functionality finsihed""
    This reverts commit 5b2ac2f6468125de191f7e57660652a3a78d0cde.

--- src/components/NavbarButton.js
+++ src/components/NavbarButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/StreamsGallery/StreamsGallery.scss';
 
 const NavbarButton = (props) => {
-	return <a tabIndex = "0" onClick = {props.handler} className = {props.className}>{props.buttonText}</a>;
+	return <button tabIndex = "0" onClick = {props.handler} className = {props.className}>{props.buttonText}</button>;
 }
 
 export default NavbarButton;
