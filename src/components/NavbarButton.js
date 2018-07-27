8ebbb13ad908544d817528bd580822ecf0ba40af
BLBaylis <bradleybaylis@hotmail.com>
2018-07-27 13:01:16 +0100

    Resting create-react-app

--- src/components/NavbarButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/StreamsGallery/StreamsGallery.scss';
-
-const NavbarButton = (props) => {
-	return <button tabIndex = "0" onClick = {props.handler} className = {props.className}>{props.buttonText}</button>;
-}
-
-export default NavbarButton;
--- src/components/RefreshBtn.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/RefreshBtn.scss';
-
-const RefreshBtn = (props) => {
-	return <button className = "refresh-btn">Refresh Streams</button>;
-};
-
-export default RefreshBtn;
