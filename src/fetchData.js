7a722a0ae3f59821869ee1e73fbe9daff099735f
BLBaylis <bradleybaylis@hotmail.com>
2018-07-31 17:31:36 +0100

    create-react-app reset

--- src/fetchData.js
+++
@@ -1,8 +0,0 @@
-export default async (streamType, streamerName) => {
-	let response = await (await fetch("https://wind-bow.glitch.me/twitch-api" + streamType + streamerName)).json();
-	if (response.status !== 200 && typeof response.status === "number") {
-		console.log("Looks like there was a problem. Status Code: " + response.status + "while trying to fetch" + streamerName + "data");
-        return false;
-    }
-    return response;
-}
